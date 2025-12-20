2133e628d216af0c71f3ebcdf0bf89c95a9e62ac
mrizwantech <mrizwantech@gmail.com>
2025-12-19 22:46:02 -0500

    Fixed splash screen , launch icon, cleanup

--- lib/src/core/errors.dart
+++
@@ -1,5 +0,0 @@
-/// App-specific error types and exceptions.
-///
-/// Responsibilities:
-/// - Define domain and data layer errors (NetworkError, NotFoundError, ParseError)
-/// - Map HTTP errors to domain-friendly error types
